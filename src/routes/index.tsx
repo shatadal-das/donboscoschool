dd098cd45463e60e04761d012eb53772a4391f23
Shatadal Das <shatadaldas.dev@gmail.com>
2025-03-11 17:28:04 +0530

    refactor: update dev script to allow host binding and remove unused Message component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,3 @@
-import Message from "@/components/message";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -6,21 +5,9 @@ export const Route = createFileRoute("/")({
 });
 
 function PageComponent() {
-  
   return (
-    <div className="text-[10vw] grid gap-12">
-      <h1 className="font-accent text-xs text-white bg-blue-600">
-        Don Bosco H.S. School, Kokrajhar
-      </h1>
-      <p className="font-primary">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo,
-        incidunt, nostrum et quae quaerat iusto eligendi tenetur vel deserunt
-        consectetur soluta quibusdam reprehenderit totam consequuntur porro
-        repudiandae amet eum! Iusto eligendi autem labore, soluta vero suscipit
-        quidem dignissimos amet quis laborum ad, fugiat porro, nulla consectetur
-        voluptatibus exercitationem saepe! Voluptate?
-      </p>
-      <Message />
+    <div>
+        
     </div>
   );
 }
